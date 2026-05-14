8920bf392c382528acd5da226fd920e88d875b1c
DongJ99108 <dongj99108@gmail.com>
2026-05-14 16:25:56 +0900

    Board7 Pds List 완료(?)

---
+++ src/main/java/com/green/pds/dto/PdsDto.java
@@ -0,0 +1,23 @@
+package com.green.pds.dto;
+
+import lombok.Data;
+
+@Data
+public class PdsDto {
+	// Board 자료
+	private int        idx;
+	private String     title;
+	private String     content;
+	private String     writer;
+	private String     regdate;
+	private int        hit;
+	
+	// Files 자료 -> 파일 수
+	private int        filescount;
+	
+	// Menus 자료
+	private String     menu_id;
+	private String     menu_name;
+	private int        menu_seq;
+	
+}
